fc69ea899c0733e4ed27c3c8cc8de2cf290c3a84
jbilee <7jujubuilds@gmail.com>
2024-08-06 15:12:19 +0000

    feat: courses 라우트 핸들러 구현 - 서버로부터 강의 목록을 fetch하는 api 라우트 핸들러 구현

--- src/components/course/CourseListContainer.tsx
+++ src/components/course/CourseListContainer.tsx
@@ -1,13 +1,31 @@
+"use client";
+
+import { useState } from "react";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import CoursePreview from "./CoursePreview";
-import { MOCK_COURSES } from "@/constants/mockdata";
+import PageNavigator from "../PageNavigator";
+import type { CourseInfoProps } from "./CourseInfo";
+
+const fetchAllCourses = (page = 1) => fetch(`/api/courses?page=${page}`).then((res) => res.json());
 
 export default function CourseListContainer() {
-  // API fetch
+  const [page, setPage] = useState(1);
+  const { data, error } = useQuery({
+    queryKey: ["all-courses", page],
+    queryFn: () => fetchAllCourses(page),
+    placeholderData: keepPreviousData,
+  });
+
+  const handlePageSelect = (value: number) => setPage(value);
+
+  if (error) return <div className="flex flex-col gap-4">에러가 발생했습니다!</div>;
+  if (!data) return <div className="w-full text-center">Loading ...</div>;
   return (
     <div className="flex flex-col gap-4">
-      {MOCK_COURSES.map((course) => (
+      {data.content.map((course: CourseInfoProps) => (
         <CoursePreview key={course.id} {...course} />
       ))}
+      <PageNavigator currentPage={data.pageNumber} pages={data.totalPages} handlePageSelect={handlePageSelect} />
     </div>
   );
 }
